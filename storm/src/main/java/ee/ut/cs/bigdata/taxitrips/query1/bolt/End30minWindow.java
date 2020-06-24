2030d14ae96ee1af7f44c1ccc47a9153980f8439
Heidi Korp <korpheidi@gmail.com>
2020-06-24 20:52:33 +0300

    Sorted routes frequency

--- storm/src/main/java/ee/ut/cs/bigdata/taxitrips/query1/bolt/End30minWindow.java
+++ storm/src/main/java/ee/ut/cs/bigdata/taxitrips/query1/bolt/End30minWindow.java
@@ -6,6 +6,7 @@ import ee.ut.cs.bigdata.taxitrips.storm.bolt.AbstractBaseWindowedBolt;
 import ee.ut.cs.bigdata.taxitrips.storm.util.ChangeTupleData;
 import ee.ut.cs.bigdata.taxitrips.storm.util.TupleDataUtil;
 import javafx.util.Pair;
+import org.apache.storm.shade.org.apache.commons.collections.ArrayStack;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
 import org.apache.storm.topology.OutputFieldsDeclarer;
@@ -17,6 +18,9 @@ import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 import java.util.*;
+
+import static java.util.Map.Entry.comparingByValue;
+import static java.util.stream.Collectors.toMap;
 
 public class End30minWindow extends AbstractBaseWindowedBolt {
 
@@ -40,10 +44,12 @@ public class End30minWindow extends AbstractBaseWindowedBolt {
         if(inputWindow.getNew().size() + inputWindow.getExpired().size() > 0) {
             List<Pair<String, String>> routes = new ArrayList<>();
             for (Tuple record : inputWindow.get()) {
+                // Calculate starting and ending cell
                 Cell startingCell = calculateStartingCell(record);
                 Cell endingCell = calculateEndingCell(record);
 
                 if (startingCell != null && endingCell != null) {
+                    // Make a route
                     Pair<String, String> route = new Pair<>(startingCell.toString(), endingCell.toString());
                     routes.add(route);
                 }
@@ -54,8 +60,8 @@ public class End30minWindow extends AbstractBaseWindowedBolt {
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer declarer) {
-        declarer.declare(new Fields("starting_cell", "ending_cell", "route_freq", "window_endtimestamp", "pickup_datetime",
-                "dropoff_datetime", "processing_starttime"));
+        declarer.declare(new Fields("starting_cell", "ending_cell", "route_freq", "pickup_datetime",
+                "dropoff_datetime", "delay"));
     }
 
     private Cell calculateStartingCell(Tuple record) {
@@ -76,6 +82,18 @@ public class End30minWindow extends AbstractBaseWindowedBolt {
         return null;
     }
 
+    private Map<Pair<String, String>, Integer> sortByValues(Map<Pair<String, String>, Integer> routes) {
+        // Sort the map by values in Ascending order
+        Map<Pair<String, String>, Integer> sorted = routes
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(
+                        toMap(e -> e.getKey(), e -> e.getValue(), (e1, e2) -> e2,
+                                LinkedHashMap::new));
+        return sorted;
+    }
+
     private void calculateAndEmitRouteFrequency(long endTimestamp, List<Pair<String, String>> routes, List<Tuple> expiredOnes, List<Tuple> newOnes) {
         // Map all routes and count their frequency
         Map<Pair<String, String>, Integer> routeMap = new HashMap<>();
@@ -87,16 +105,15 @@ public class End30minWindow extends AbstractBaseWindowedBolt {
                 routeMap.put(route, 1);
             }
         }
+        // Sort routes by their frequency
+        Map<Pair<String, String>, Integer> sortedRoutes = sortByValues(routeMap);
+        // Save routes and their frequencies as lists
+        List<Pair<String, String>> cells = new ArrayList<>(sortedRoutes.keySet());
+        List<Integer> routeFreq = new ArrayList<>(sortedRoutes.values());
 
-        // Make a list of route frequency corresponding to each entry
-        List<Integer> counts = new ArrayList<>();
-        for (Pair<String, String> route : routes) {
-            int value = routeMap.get(route);
-            counts.add(value);
-        }
-        emitForOneChange(endTimestamp, routes, counts, expiredOnes, newOnes);
+        emitForOneChange(endTimestamp, cells, routeFreq, expiredOnes, newOnes);
     }
-    //todo redesign
+
     private void emitForOneChange(long endTimestamp, List<Pair<String, String>> routes, List<Integer> counts, List<Tuple> expiredOnes, List<Tuple> newOnes) {
         issueWarningIfMultipleChanges(expiredOnes, newOnes);
 
@@ -110,15 +127,22 @@ public class End30minWindow extends AbstractBaseWindowedBolt {
             processingStarttime = changeTupleData.processingStarttime;
         }
 
-        emitCellsAndRouteFreq(endTimestamp, pickupDatetime, dropoffDatetime, processingStarttime, routes, counts);
+        emitCellsAndRouteFreq(pickupDatetime, dropoffDatetime, processingStarttime, routes, counts);
     }
 
 
-    protected void emitCellsAndRouteFreq(long endTimestamp, String pickupDatetime, String dropoffDatetime,
+    protected void emitCellsAndRouteFreq(String pickupDatetime, String dropoffDatetime,
                                          Long processingStarttime, List<Pair<String, String>> routes, List<Integer> counts) {
-        for (int i = 0; i < routes.size(); i++) {
-            collector.emit(Arrays.asList(routes.get(i).getKey(), routes.get(i).getValue(), counts.get(i), endTimestamp,
-                    pickupDatetime, dropoffDatetime, processingStarttime));
+        // Loop over routes in reverse order to get top 10 frequent routes
+        for (int i = routes.size() - 1; i >= 0; i--) {
+            if (i > routes.size() - 11) {
+                // If there are less that 10 routes emitted, emit more
+                collector.emit(Arrays.asList(routes.get(i).getKey(), routes.get(i).getValue(), counts.get(i),
+                        pickupDatetime, dropoffDatetime, System.currentTimeMillis() - processingStarttime));
+            } else {
+                // If the record does not make it into top 10 most frequent routes, emit nulls
+                collector.emit(Arrays.asList(null, null, null, null, null, null));
+            }
         }
     }
 
